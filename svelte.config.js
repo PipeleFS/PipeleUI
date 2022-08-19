e710d0e88a0c4ed3f62acfa05a4994522fd43757
michaelvogt <info@michaselvogt.eu>
2022-08-19 10:14:53 +0200

    Use netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false
 		}),
 
 		// Override http methods in the Todo forms
